e133e32651e34ce58f16dbb100c39abe4b50b1ab
mcclenaghanmark91 <58256834+mcclenaghanmark91@users.noreply.github.com>
2024-01-11 00:35:12 +0000

    72. Template Refs in the Setup function

--- Section-9-The-Composition-API/dojo-blog/src/views/HomeView.vue
+++ Section-9-The-Composition-API/dojo-blog/src/views/HomeView.vue
@@ -1,26 +1,33 @@
 <template>
   <div class="home">
     HOME
-    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">click me</button>
   </div>
 </template>
 
 <script>
+import {ref} from 'vue'
 
 export default {
   name: 'HomeView',
   setup() {
+    
+    const p = ref(null)
+    console.log(p, p.value, 'uh oh')
+
     console.log('setup')
     //these aren't reactive values but can be made to be so
     let name = 'Mario'
     let age = 30
 
     const handleClick = () => {
-      console.log('you clicked me')
+      console.log(p, p.value)
+      p.value.classList.add('test')
+      p.value.textContent = 'hello, ninjas'
     }
 
-    return { name, age, handleClick }
+    return { name, age, handleClick, p }
   }
 }
 </script>
