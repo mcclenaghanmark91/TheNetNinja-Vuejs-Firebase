9a5f6f2314a7ad6b32985b3f12d0651905bc8674
mcclenaghanmark91 <58256834+mcclenaghanmark91@users.noreply.github.com>
2024-01-15 00:39:33 +0000

    81. Creating another composable - 82. Creating a spinner

--- Section-9-The-Composition-API/dojo-blog/src/views/HomeView.vue
+++ Section-9-The-Composition-API/dojo-blog/src/views/HomeView.vue
@@ -5,7 +5,9 @@
     <div v-if="posts.length">
       <PostList :posts="posts"/>
     </div>
-    <div v-else>Loading...</div>
+    <div v-else>
+      <Spinner />
+    </div>
   </div>
 </template>
 
@@ -13,10 +15,11 @@
 
 import PostList from '../components/PostList.vue'
 import getPosts from '../composables/getPosts'
+import Spinner from '../components/Spinner.vue'
 
 export default {
   name: 'HomeView',
-  components: {PostList},
+  components: {PostList, Spinner},
   setup() {
 
     const { posts, error, load} = getPosts()
