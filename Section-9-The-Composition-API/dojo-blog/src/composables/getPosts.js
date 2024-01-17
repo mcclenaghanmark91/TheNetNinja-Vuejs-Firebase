e927fbc238dc43f4c638ddaba7bbf90546bfac15
mcclenaghanmark91 <58256834+mcclenaghanmark91@users.noreply.github.com>
2024-01-17 12:05:25 +0000

    85. Future Async lesson and extra styling
    Suspense tag coming in a future video when its not experimental.

--- Section-9-The-Composition-API/dojo-blog/src/composables/getPosts.js
+++ Section-9-The-Composition-API/dojo-blog/src/composables/getPosts.js
@@ -6,10 +6,7 @@ const getPosts = () => {
 
     const load = async () => {
       try {
-        //simulate delay
-        await new Promise(resolve => {
-            setTimeout(resolve, 2000)
-        })
+
 
         let data = await fetch('http://localhost:3000/posts')
         if (!data.ok) {
