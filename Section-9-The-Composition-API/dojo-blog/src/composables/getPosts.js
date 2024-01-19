c81f2f8611745bda4265956448b62efc54e98094
mcclenaghanmark91 <58256834+mcclenaghanmark91@users.noreply.github.com>
2024-01-19 12:19:07 +0000

    93. Fetching a collection of documents

--- Section-9-The-Composition-API/dojo-blog/src/composables/getPosts.js
+++ Section-9-The-Composition-API/dojo-blog/src/composables/getPosts.js
@@ -1,4 +1,5 @@
 import { ref } from 'vue'
+import { projectFirestore } from '../Firebase/config'
 
 const getPosts = () => {
     const posts = ref([])
@@ -6,13 +7,12 @@ const getPosts = () => {
 
     const load = async () => {
       try {
-
-
-        let data = await fetch('http://localhost:3000/posts')
-        if (!data.ok) {
-          throw Error('no data available')
-        }
-        posts.value = await data.json() 
+        const res = await projectFirestore.collection('posts').get()
+        
+        posts.value = res.docs.map(doc => {
+          //console.log(doc.data())
+          return { ...doc.data(), id: doc.id }
+        })
       }
       catch (err) {
         error.value = err.message
