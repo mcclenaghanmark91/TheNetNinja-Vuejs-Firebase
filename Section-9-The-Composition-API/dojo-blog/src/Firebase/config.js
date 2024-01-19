46c9cf70e6c655344722d3563203cbd10dda098f
mcclenaghanmark91 <58256834+mcclenaghanmark91@users.noreply.github.com>
2024-01-19 12:00:03 +0000

    92. Connecting to a firebase project

---
+++ Section-9-The-Composition-API/dojo-blog/src/Firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB8O8xGPoc4i7AEFRNyQpw1BnP6n9rtKOc",
+    authDomain: "udemy-vue-firebase-sites-8e549.firebaseapp.com",
+    projectId: "udemy-vue-firebase-sites-8e549",
+    storageBucket: "udemy-vue-firebase-sites-8e549.appspot.com",
+    messagingSenderId: "216408098491",
+    appId: "1:216408098491:web:173aebaea4cae46d488b9e"
+  };
+
+  //init firebase
+  firebase.initializeApp(firebaseConfig)
+
+  //init firestore service
+  const projectFirestore = firebase.firestore()
+
+  export { projectFirestore }
