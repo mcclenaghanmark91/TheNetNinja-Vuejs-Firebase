cf3b806f9ef735a2129dd0b9c10f71836c758777
mcclenaghanmark91 <58256834+mcclenaghanmark91@users.noreply.github.com>
2024-01-21 16:25:24 +0000

    user signup

--- Section-13-PROJECT-Live-Chatroom/live-chat/src/components/SignupForm.vue
+++ Section-13-PROJECT-Live-Chatroom/live-chat/src/components/SignupForm.vue
@@ -1,4 +1,4 @@
-<template lang="">
+<template>
     <form @submit.prevent="handleSubmit">
         <input type="text" required placeholder="display name" v-model="displayName">
         <input type="email" required placeholder="email" v-model="email">
@@ -8,22 +8,26 @@
 </template>
 <script>
 import { ref } from 'vue'
+import useSignup from '../composables/useSignup'
+
 
 export default {
     setup(){
+        const { error, signup} = useSignup()
         //refs
         const displayName = ref('')
         const email = ref('')
         const password = ref('')
 
-        const handleSubmit = () => {
-            console.log(displayName.value, email.value, password.value)
+        const handleSubmit = async () => {
+            await signup(email.value, password.value, displayName.value)
+            console.log('user signed up')
         }
 
         return {  displayName, email, password, handleSubmit }
     }
 }
 </script>
-<style lang="">
+<style>
     
 </style>
