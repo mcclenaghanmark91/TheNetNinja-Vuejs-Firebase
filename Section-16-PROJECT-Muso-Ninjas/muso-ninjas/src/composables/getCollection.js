45cc90f3f06ca2ba2995c88478de2b2a78cd2e8f
mcclenaghanmark91 <58256834+mcclenaghanmark91@users.noreply.github.com>
2024-02-06 17:21:32 +0000

    fixing bugs

--- Section-16-PROJECT-Muso-Ninjas/muso-ninjas/src/composables/getCollection.js
+++ Section-16-PROJECT-Muso-Ninjas/muso-ninjas/src/composables/getCollection.js
@@ -1,36 +1,40 @@
-import { ref, watchEffect } from 'vue';
-import { projectFirestore } from '../firebase/config';
+import { ref, watchEffect } from 'vue'
+import { projectFirestore } from '../firebase/config'
 
 const getCollection = (collection, query) => {
 
-    const documents = ref(null)
-    const error = ref(null)
+  const documents = ref(null)
+  const error = ref(null)
 
-    let collectionRef = projectFirestore.collection(collection)
-        .orderBy('createdAt')
+  // register the firestore collection reference
+  let collectionRef = projectFirestore.collection(collection)
+    .orderBy('createdAt')
 
-    if(query) {
-        collectionRef = collectionRef.where(...query)
-    }
+  if (query) {
+    collectionRef = collectionRef.where(...query)
+  }
 
-    const unsub = collectionRef.onSnapshot((snap) => {
-        let results = []
-        snap.docs.forEach(doc => {
-            doc.data().createdAt && results.push({ ...doc.data(), id: doc.id })
-        });
-        documents.value = results
-        error.value = null
-    }, (err) => {
-        documents.value = null
-        error.value = 'Could not fetch data'
-    })
+  const unsub = collectionRef.onSnapshot(snap => {
+    let results = []
+    snap.docs.forEach(doc => {
+      // must wait for the server to create the timestamp & send it back
+      doc.data().createdAt && results.push({...doc.data(), id: doc.id})
+    });
+    
+    // update values
+    documents.value = results
+    error.value = null
+  }, err => {
+    console.log(err.message)
+    documents.value = null
+    error.value = 'could not fetch the data'
+  })
 
-    watchEffect((onInvalidate) => {
-        //unsub from prev collection when watcher is stopped (component unmounted)
-        onInvalidate(() => unsub())
-    })
+  watchEffect((onInvalidate) => {
+    onInvalidate(() => unsub());
+  });
 
-    return { error, documents}
+  return { error, documents }
 }
 
 export default getCollection
