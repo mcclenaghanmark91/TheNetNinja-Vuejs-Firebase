a8137c322b9c0343c4379c2c6a2c02103416bf11
mcclenaghanmark91 <58256834+mcclenaghanmark91@users.noreply.github.com>
2024-02-06 17:15:49 +0000

    Setting up MyPlaylists page

--- Section-16-PROJECT-Muso-Ninjas/muso-ninjas/src/views/playlists/UserPlaylists.vue
+++ Section-16-PROJECT-Muso-Ninjas/muso-ninjas/src/views/playlists/UserPlaylists.vue
@@ -1,12 +1,30 @@
 <template>
     <div class="user-playlists">
         <h2>My Playlists</h2>
+        <div v-if="playlists">
+            <ListView :playlists="playlists"/>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
+import getUser from '@/composables/getUser';
+import getCollection from '@/composables/getCollection'
+import ListView from '@/components/ListView.vue'
 
+export default {
+    components: { ListView },
+    setup(){
+        const { user } = getUser();
+        const { documents: playlists } = getCollection(
+            'playlists',
+            ['userId', '==', user.value.uid]
+        );
+
+        console.log("playlists:" ,playlists);
+
+        return { playlists }
+    }
 }
 </script>
 
